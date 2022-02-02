6a321e5923071c1449c69c45d4b304f4ec960354
Kraots <70799557+Kraots@users.noreply.github.com>
2022-02-02 22:50:52 +0200

    supposed to be a tuple containing the symbol name as well

--- docs/cog.py
+++ docs/cog.py
@@ -229,7 +229,7 @@ class Docs(commands.Cog):
         doc_symbols = list(self.doc_symbols.items())
         matches = finder(symbol_name, doc_symbols, key=lambda t: t[0], lazy=False)[:self.limit]
         if doc_item is not None:
-            matches = [doc_item, *matches]
+            matches = [(symbol_name, doc_item), *matches]
         return matches
 
     async def get_symbol_markdown(self, doc_item: DocItem) -> str:
